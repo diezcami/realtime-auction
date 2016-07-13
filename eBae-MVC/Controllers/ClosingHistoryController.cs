ee9399e118885e137d30aa4f1fde67ddb35b3a2f
Cami Diez <diezcami@gmail.com>
2016-07-13 15:02:57 +0800

    Added view won auctions page

---
+++ eBae-MVC/Controllers/ClosingHistoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eBae_MVC.Models;
+using eBae_MVC.DAL;
+
+namespace eBae_MVC.Controllers
+{
+    public class ClosingHistoryController : Controller
+    {
+        private AuctionContext db = new AuctionContext();
+
+        // GET: /ClosingHistory/
+        public ActionResult Index()
+        {
+            int userID = Convert.ToInt32(Session["CurrentUserID"]);
+            var closinghistories = db.ClosingHistories.Where(ch => ch.UserID == userID);
+            return View(closinghistories.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
